cc32db506647e1480981aede4426409800f383b2
Srinivas Garlapati <srinivasa.b.garlapati@gmail.com>
2015-04-26 21:00:38 +0530

    Java sequential loop time check

---
+++ java_threads/SeqRun.java
@@ -0,0 +1,24 @@
+// Time test for a sequential loop counter
+
+
+import java.util.Date;
+
+
+public class SeqRun {
+
+    public static void main(String[] args){
+
+        int loop_counter = 1;
+        int count = 50000000;
+        long start_time = System.currentTimeMillis();
+
+        while(loop_counter <= count) {
+            loop_counter--;
+        }
+        long end_time = System.currentTimeMillis();
+        long diff_time = end_time - start_time;
+
+        System.out.println("Time taken in milli seconds " + diff_time);
+    }
+}
+
